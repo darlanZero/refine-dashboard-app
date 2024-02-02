0a5fab3503b6fe88efeaaa4f79792450c81f81d2
darlanZero <darliankeira229@gmail.com>
2024-02-01 21:45:16 -0300

    dataProvider & liveProvider passed

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider } from "./providers";
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -24,8 +21,8 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                //dataProvider={dataProvider(gqlClient)}
-                //liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 //authProvider={}
--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,6 +1,13 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphQlDataProvider, { 
+    GraphQLClient,
+    liveProvider as graphQlLiveProvider 
+} from "@refinedev/nestjs-query";
+import { createClient } from "graphql-ws";
+import { fetchWrapper } from "./fetch-wrapper";
 
+export const API_BASE_URL = "https://api.crm.refine.dev"
 export const API_URL = "https://api.crm.refine.dev"
+export const WS_URL = "wss://api.crm.refine.dev/graphql"
 
 export const client = new GraphQLClient(API_URL, {
     fetch: (url: string, options: RequestInit) => {
@@ -11,3 +18,20 @@ export const client = new GraphQLClient(API_URL, {
         }
     }
 })
+
+export const wsClient = typeof window !== "undefined"
+ ? createClient({
+    url: WS_URL,
+    connectionParams: () => {
+        const accessToken = localStorage.getItem("access_token");
+
+        return {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }
+    }
+ }): undefined
+
+export const dataProvider = graphQlDataProvider(client)
+export const liveProvider = wsClient ? graphQlLiveProvider(wsClient) : undefined
