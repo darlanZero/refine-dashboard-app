56290e94dbdee0432dee425bfb67e4d6dc4fa6ac
darlanZero <darliankeira229@gmail.com>
2024-02-09 23:09:30 -0300

    upcoming events created succesfully

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,13 +1,35 @@
 import { CalendarOutlined } from '@ant-design/icons'
 import { Badge, Card, List } from 'antd'
-import React, { useState } from 'react'
 import { Text } from '../text'
 import UpcomingEventsSkeleton from '../skeleton/upcoming-events'
 import { getDate } from '@/utilities/helpers'
+import { useList } from '@refinedev/core'
+import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from '@/graphql/queries'
+import dayjs from 'dayjs'
 
 const UpcomingEvents = () => {
 
-  const [isLoading, setIsLoading] = useState(false)
+  const {data, isLoading} = useList({
+    resource: 'events',
+    pagination: {pageSize: 5},
+    sorters: [
+        {
+            field: 'startDate',
+            order: 'asc'
+        }
+    ],
+    filters:[
+        {
+            field: 'startDate',
+            operator: 'gte',
+            value: dayjs().format('YYYY-MM-DD')
+        }
+    ],
+    meta: {
+        gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY
+    }
+  })
+
   return (
     <Card 
         style={{height: '100%'}} 
@@ -39,7 +61,7 @@ const UpcomingEvents = () => {
         ): (
             <List
             itemLayout='horizontal'
-            dataSource={[]}
+            dataSource={data?.data || []}
             renderItem={(item => {
 
                 const renderDate = getDate(item.startDate, item.endDate)
@@ -58,6 +80,19 @@ const UpcomingEvents = () => {
                 )
             })}
             />
+        )}
+
+        {!isLoading && data?.data.length === 0 && (
+            <span
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '220px'
+                }}
+            >
+                No upcoming events
+            </span>
         )}
     </Card>
   )
