283ab372b06256f79ef0ab3f1791902b901cb735
darlanZero <darliankeira229@gmail.com>
2024-02-09 23:23:33 -0300

    base ui for deals chart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { DollarOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
+import { Text } from '../text'
+import { Area } from '@ant-design/plots'
 
 const DealsChart = () => {
+
+  const config = {
+
+  }
+
   return (
-    <div>DealsChart</div>
+    <Card
+      style={{
+        height: '100%',
+      }}
+
+      headStyle={{
+        padding: '8px 16px'
+      }}
+
+      bodyStyle={{
+        padding: '24px 24px 0 24px'
+      }}
+
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px'
+          }}
+        >
+          <DollarOutlined />
+
+          <Text
+            size='sm'
+            style={{
+              marginLeft: '0.5rem'
+            }}
+          >
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325}/>
+    </Card>
   )
 }
 
