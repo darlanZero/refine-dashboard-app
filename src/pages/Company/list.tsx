005fdd106558cae39477222d0f0b33f34f07ef90
darlanZero <darliankeira229@gmail.com>
2024-02-20 22:42:38 -0300

    list finished

--- src/pages/Company/list.tsx
+++ src/pages/Company/list.tsx
@@ -1,7 +1,12 @@
+import CustomAvatar from "@/components/custom-avatar"
+import { Text } from "@/components/text"
 import { COMPANIES_LIST_QUERY } from "@/graphql/queries"
-import { CreateButton, List, useTable } from "@refinedev/antd"
-import { useGo } from "@refinedev/core"
-import { Table } from "antd"
+import { Company } from "@/graphql/schema.types"
+import { currencyNumber } from "@/utilities"
+import { SearchOutlined } from "@ant-design/icons"
+import { CreateButton, DeleteButton, EditButton, FilterDropdown, List, useTable } from "@refinedev/antd"
+import { getDefaultFilter, useGo } from "@refinedev/core"
+import { Input, Space, Table } from "antd"
 
 export const CompanyList = () => {
 
@@ -43,9 +48,54 @@ export const CompanyList = () => {
           ...tableProps.pagination
         }}
       >
-        <Table.Column 
+        <Table.Column<Company> 
           dataIndex="name"
           title="Company Title"
+          defaultFilteredValue={getDefaultFilter('id', filters)}
+          filterIcon={<SearchOutlined />}
+          filterDropdown={(props) => (
+            <FilterDropdown {...props}>
+              <Input placeholder="Search Company" />
+            </FilterDropdown>
+          )}
+          render={(value, record) => (
+            <Space>
+              <CustomAvatar shape="square" name={record.name} src={record.avatarUrl} />
+              <Text style={{ whiteSpace: 'nowrap'}}>
+                {record.name}
+              </Text>
+            </Space>
+          )}
+        />
+        <Table.Column<Company>
+          dataIndex="totalRevenue" 
+          title="Open Deals Amount" 
+          render={(value, company) => (
+            <Text>
+              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+            </Text>
+          )}
+        />
+
+        <Table.Column<Company>
+          dataIndex="id" 
+          title="Actions" 
+          fixed='right'
+          render={(value) => (
+            <Space>
+              <EditButton 
+                hideText
+                size="small"
+                recordItemId={value}
+              />
+
+              <DeleteButton 
+                hideText
+                size="small"
+                recordItemId={value}
+              />
+            </Space>
+          )}
         />
       </Table>
     </List>
